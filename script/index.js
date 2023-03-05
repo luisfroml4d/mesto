3a2b3ae6e6f11c9fff3d791ca9a5aaf76c87b129
luisfroml4d <luisleft2@gmail.com>
2023-03-05 16:11:41 +0300

    feature/ добавление addButton

--- script/index.js
+++ script/index.js
@@ -10,17 +10,22 @@ let nameInfo = document.querySelector('.profile__title');
 let jobInput = document.querySelector('.popup__input_name_profession');
 let jobInfo = document.querySelector('.profile__description');
 
+let addButton = document.querySelector('.profile__add-button');
 
+
+
+
+//открытие попапа Эдит//
 function openPopup() {
   popup.classList.add('popup_opened');
   nameInput.value = nameInfo.textContent;
   jobInput.value = jobInfo.textContent;
 }
-
+//закрытие попапа Эдит//
 function closePopup() {
   popup.classList.remove('popup_opened');
 };
-
+//кнопна сохранить в попапе Эдит батон//
 function handleFormSubmit(evt) {
   evt.preventDefault();
   nameInfo.textContent = nameInput.value;
@@ -28,13 +33,13 @@ function handleFormSubmit(evt) {
   closePopup();
 };
 
-
+//колбэк на открытие попапа Эдит//
 editButton.addEventListener('click', function() {
   openPopup();
 });
-
+//колбэк на закрытие попапа Эдит//
 closeButton.addEventListener('click', function () {
   closePopup();
 });
-
+//колбэк на кнопку сохранить в Эдит попап Эдит//
 formElement.addEventListener('submit', handleFormSubmit);
